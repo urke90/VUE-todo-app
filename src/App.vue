5e5f30e302401575c2a9c8f1eb1a0fbcc5a09c2c
urke90 <urosbijelic90@gmail.com>
2021-03-05 12:58:51 +0100

    implemented header component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+    <the-header title="VUE ToDo"></the-header>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import TheHeader from './components/header/TheHeader.vue';
 
 export default {
-  name: "App",
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {
+        TheHeader
+    }
 };
 </script>
 
 <style>
+* {
+    margin: 0;
+    padding: 0;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
 }
 </style>
---
+++ src/components/header/TheHeader.vue
@@ -0,0 +1,26 @@
+<template>
+    <header>
+        <h2>
+            {{ title }}
+        </h2>
+    </header>
+</template>
+
+<script>
+export default {
+    props: {
+        title: {
+            type: String,
+            required: true
+        }
+    }
+};
+</script>
+
+<style scoped>
+h2 {
+    background-color: #f59c2f;
+    text-align: center;
+    padding: 20px;
+}
+</style>
